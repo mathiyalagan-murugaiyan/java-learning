df678803a4dfc7c0bad10586e2fe38710ff78805
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-09-30 16:51:21 +0530

    Array binary search

--- data_structurs_in_java/src/data_structurs_in_java/ArrayBinarySearch.java
+++ data_structurs_in_java/src/data_structurs_in_java/ArrayBinarySearch.java
@@ -11,7 +11,9 @@ public class ArrayBinarySearch {
 		int target=in.nextInt();
 		
 		
-		int[]arr= {1,2,3,4,5,6,7,8,9,10,14,22,34};
+		//int[]arr= {1,2,3,4,5,6,7,8,9,10,14,22,34};
+		
+		int[]arr= {22,14,11,9,8,5,4,3,2,1};
 		
 	System.out.println(ArrayBinarySearch_Method.find(arr,target));
 
--- data_structurs_in_java/src/data_structurs_in_java/ArrayBinarySearch_Method.java
+++ data_structurs_in_java/src/data_structurs_in_java/ArrayBinarySearch_Method.java
@@ -3,23 +3,34 @@ package data_structurs_in_java;
 public class ArrayBinarySearch_Method {
 
 	public static int find(int[] arr, int target) {
-		
-		int start=0;
-		
-		int end = arr.length-1;
-		while(start<=end) {
-			
-			int mid = (start+end)/2;
-			
-			if(target>arr[mid]) 
-				start=mid+1;
-			
-			else if(target<arr[mid]) 
-				end=mid-1;
-			else
-			return mid;
+
+		int start = 0;
+
+		int end = arr.length - 1;
+
+		boolean asc = arr[start] < arr[end];
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+
+			if (asc) {
+				if (target > arr[mid])
+					start = mid + 1;
+
+				else if (target < arr[mid])
+					end = mid - 1;
+				else
+					return mid;
+			} else {
+				if (target > arr[mid])
+					end = end - 1;
+
+				else if (target < arr[mid])
+					start = start + 1;
+				else
+					return mid;
+			}
 		}
 		return -1;
 	}
-
 }
