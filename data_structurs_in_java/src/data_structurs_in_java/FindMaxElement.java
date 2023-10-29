17f5b50b4a5f41586de056078cc426c6c0627919
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-10-29 23:33:41 +0530

    find max by bubble sort

--- data_structurs_in_java/src/data_structurs_in_java/FindMaxElement.java
+++ data_structurs_in_java/src/data_structurs_in_java/FindMaxElement.java
@@ -4,7 +4,7 @@ public class FindMaxElement {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 10, 8, 5, 2, 1 };
+		int[] arr = { 10, 8, 5, 2,7,9, 1 };
 
 		findMax(arr);
 
@@ -16,7 +16,7 @@ public class FindMaxElement {
 		for (int i = 1; i < arr.length; i++) {
 			for (int j = 0; j < arr.length - i; j++) {
 
-				if (arr[j] < arr[j + 1]) {
+				if (arr[j] > arr[j + 1]) {
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
