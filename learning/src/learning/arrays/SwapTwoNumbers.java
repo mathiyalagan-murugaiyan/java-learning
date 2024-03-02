22457e73e3153cd9c904ba419bd066021d4c7be3
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2024-03-02 22:48:29 +0530

    practice

---
+++ learning/src/learning/arrays/SwapTwoNumbers.java
@@ -0,0 +1,21 @@
+package learning.arrays;
+
+public class SwapTwoNumbers {
+	
+	public static void main(String args[] ){
+	
+		int a= 5;
+		int b = 10;
+		
+		System.out.println(a);
+		System.out.println(b);
+		a= a+b;
+		
+		b= a- b;
+		a=a-b;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
